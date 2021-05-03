b7bca8a424d01dcf0a6acb4c67f20ea50798d304
webdornald <webdornald@gmail.com>
2021-05-04 00:18:25 +0900

    서머노트 에디터 적용

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ from django.urls import reverse_lazy
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = 'django-insecure-)q9y#e2%u+*0=yiwe^y&wl=1^mwc74x&@@e27j#is6=b=p)^)s
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -77,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -87,7 +84,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -107,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -120,7 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -145,5 +139,9 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 SUMMERNOTE_CONFIG = {
     'summernote': {
         'width': '100%',
+        'attachment_filesize_limit': 1024 * 1024 * 5,
+        'lang': 'ko-KR',
     }
 }
+
+SUMMERNOTE_THEME = 'bs4'
--- projectapp/admin.py
+++ projectapp/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django_summernote.admin import SummernoteModelAdmin
+
 from projectapp.models import Project
 
-admin.site.register(Project)
+
+class ProjectModelAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+
+admin.site.register(Project, ProjectModelAdmin)
