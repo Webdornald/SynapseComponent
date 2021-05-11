c24907ab3b04105eb49b43429408374d8001394a
webdornald <webdornald@gmail.com>
2021-05-11 11:38:52 +0900

    개발용과 배포용 도커 분리하기

--- accountapp/views.py
+++ accountapp/views.py
@@ -24,7 +24,7 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
     def form_valid(self, form):
-        user = form.save()
+        user = form.save(commit=False)
         Profile.objects.create(user=user)
         return super().form_valid(form)
 
--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,8 +130,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / '_static'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / '_media'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 LOGIN_REDIRECT_URL = reverse_lazy('informationapp:index')
 LOGOUT_REDIRECT_URL = reverse_lazy('informationapp:index')
