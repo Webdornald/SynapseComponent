8bb7816869d270f12469f1516c84f0d517beb029
webdornald <webdornald@gmail.com>
2021-05-12 16:10:32 +0900

    도커 파일 임시 완성

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.9.0
+
+WORKDIR /home/
+
+RUN git clone https://github.com/Webdornald/SynapseComponent.git
+
+WORKDIR /home/SynapseComponent/
+
+RUN pip install -r requirements.txt
+
+RUN echo 'SECRET_KEY=django-insecure-)q9y#e2%u+*0=yiwe^y&wl=1^mwc74x&@@e27j#is6=b=p)^)s' > .env
+
+RUN python manage.py migrate
+
+EXPOSE 8000
+
+CMD ['python', 'manage.py', 'runserver', '0.0.0.0:8000']
--- config/settings.py
+++ config/settings.py
@@ -12,13 +12,25 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
 
+import environ, os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)q9y#e2%u+*0=yiwe^y&wl=1^mwc74x&@@e27j#is6=b=p)^)s'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
