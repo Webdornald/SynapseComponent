38ff80f3287b7f97cc1ecd135f0bac3140f975cb
Webdornald <webdornald@gmail.com>
2021-11-11 16:32:18 +0900

    개인정보 처리방침 페이지 추가

--- informationapp/urls.py
+++ informationapp/urls.py
@@ -4,5 +4,6 @@ import informationapp.views as views
 app_name = 'informationapp'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('privacypolicy/', views.privacypolicy, name='privacypolicy')
 ]
--- informationapp/views.py
+++ informationapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'infomationapp/index.html')
+
+
+def privacypolicy(request):
+    return render(request, 'infomationapp/privacy_policy.html')
