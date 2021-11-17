f9412053f9bf619bdca02594b3af841c759b867d
Webdornald <webdornald@gmail.com>
2021-11-17 17:24:08 +0900

    d-day 표시

--- informationapp/views.py
+++ informationapp/views.py
@@ -1,9 +1,15 @@
+import datetime
+
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'infomationapp/index.html')
+    today = datetime.date.today()
+    target_day = datetime.date(2031, 11, 7)
+    d_day = target_day - today
+    complete_day = d_day.days
+    return render(request, 'infomationapp/index.html', {'complete_time': complete_day})
 
 
 def privacypolicy(request):
